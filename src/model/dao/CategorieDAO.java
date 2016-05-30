5a0ccbacae95272585081acc799e76f81fa7e7e0
Knudian <3Lra!s3r>
2016-05-30 22:40:40 +0200

    Finishing CategorieDAO

--- src/model/dao/CategorieDAO.java
+++ src/model/dao/CategorieDAO.java
@@ -8,10 +8,9 @@ package model.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.beans.Categorie;
 
 /**
@@ -54,18 +53,55 @@ public class CategorieDAO extends DAO<Categorie> {
     }
 
     @Override
-    public Categorie create(Categorie o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Categorie create(Categorie cat) {
+        this.setChanged();
+        String sql = "INSERT INTO categorie (libelle, libelle_court, id_groupe, id_admintech) VALUES (?,?,?,?)";
+        PreparedStatement pst;
+        try {
+            pst = connexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pst.setString(1, cat.getLibelle());
+            pst.setString(2, cat.getLibelle_court());
+            pst.setInt(3, cat.getIdGroupe());
+            pst.setInt(4, cat.getReferent());
+            pst.executeUpdate();
+            ResultSet rs = pst.getGeneratedKeys();
+        } catch (SQLException ex) {
+            this.notifyObservers(ex);
+            ex.printStackTrace();
+        }
+        this.notifyObservers(cat);
+        return cat;
     }
 
     @Override
-    public Categorie update(Categorie o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Categorie update(Categorie cat) {
+        String sql = "UPDATE INTO categorie SET libelle=?, libelle_court=?, id_groupe=?, id_admintech=? WHERE id_categorie=?";
+        PreparedStatement pst;
+        try {
+            pst = connexion.prepareStatement(sql);
+            pst.setString(1, cat.getLibelle());
+            pst.setString(2, cat.getLibelle_court());
+            pst.setInt(3, cat.getIdGroupe());
+            pst.setInt(4, cat.getReferent());
+            pst.setInt(5, cat.getIdCategorie());
+            ResultSet rs = pst.executeQuery();
+        } catch (SQLException ex) {
+            this.notifyObservers(ex);
+            ex.printStackTrace();
+        }
+        return cat;
     }
 
     @Override
-    public void delete(Categorie o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(Categorie cat) {
+        try {
+            String sql = "DELETE FROM categorie WHERE id_categorie=?";
+            PreparedStatement pst = connexion.prepareStatement(sql);
+            pst.setInt(1, cat.getIdCategorie());
+            pst.executeQuery();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public List<Categorie> findCategoriesForFilters() {
